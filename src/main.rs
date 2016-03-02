a1180069d3636b5a67aa2d068689a95b4189e04c
vrinek <vrinek@users.noreply.github.com>
2016-03-02 21:47:20 +0000

    Choose a card to turn it face up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+extern crate rand;
+use rand::Rng;
+use std::io;
+use std::fmt;
+use std::io::Write;
+
+#[derive(Debug)]
+enum Facing {
+    Up,
+    Down,
+}
+
+#[derive(Debug)]
+struct Card {
+    facing: Facing,
+    number: u8,
+}
+
+impl Card {
+    fn new(number: u8) -> Card {
+        Card { facing: Facing::Down, number: number }
+    }
+    
+    fn turn_up(&mut self) {
+        self.facing = Facing::Up;
+    }
+}
+
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self.facing {
+            Facing::Up => write!(f, "[{}]", self.number),
+            Facing::Down => write!(f, "[  ]"),
+        }
+        
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut deck: Vec<Card> = vec![];
+    for i in 1..9 {
+        deck.push(Card::new(i));
+        deck.push(Card::new(i));
+    }
+
+    let mut rng = rand::weak_rng();
+    rng.shuffle(&mut deck);
+    drop(rng);
+
+    loop {
+        display_deck(&deck);
+
+        println!("Please enter a number from 1-18:");
+
+        let mut picked_card_index = String::new();
+        io::stdin().read_line(&mut picked_card_index)
+            .expect("Failed to read line");
+
+        let picked_card_index: usize = match picked_card_index.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        deck[picked_card_index - 1].turn_up();
+    }
+}
+
+fn display_deck(deck: &Vec<Card>) {
+    for card in deck {
+        print!("{}", card);
+    }
+    print!("\n");
 }
